8562e8f7d49495d23693e4c96962ad0d6fca015d
Michael Tarleton <michael.tarleton@me.com>
2018-12-25 22:12:02 -0600

    master: Added router outlet

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core'
 import {} from '@angular/material'
 
+import { RouterModule } from '@angular/router'
 import { SharedModule } from '../shared/shared.module'
 import { NavbarComponent } from './navbar/navbar.component'
 
 @NgModule({
-  imports: [SharedModule],
+  imports: [SharedModule, RouterModule],
   exports: [SharedModule, NavbarComponent],
   declarations: [NavbarComponent],
 })
