92806eb3493f07491c978fcd6e95ff68f90c3118
Michael Tarleton <michael.tarleton@me.com>
2018-12-25 21:34:22 -0600

    master: Angular material setting up imports and exports

--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+
+const modules: ReadonlyArray<any> = []
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [...modules],
+  exports: [...modules],
 })
-export class AngularMaterialModule { }
+export class AngularMaterialModule {}
