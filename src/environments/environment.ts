10be98179215c21297c6576ca7edea26769f5b5d
Michael Tarleton <michael.tarleton@me.com>
2018-12-25 21:51:45 -0600

    master: Added api url to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  api: 'https://localhost:5001/api/',
+}
 
 /*
  * In development mode, for easier debugging, you can ignore zone related error
