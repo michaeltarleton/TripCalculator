e0cd873390b656dd765bdbff89890bccda12dfc8
Michael Tarleton <michael.tarleton@me.com>
2018-12-25 22:23:28 -0600

    master: Init friend list dashboard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { FriendListDashboardComponent } from './modules/friend-list-dashboard/friend-list-dashboard.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'friends',
+  },
+
+  {
+    path: 'friends',
+    component: FriendListDashboardComponent,
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
