fe53cc2449211a0a9a24a5d84545dec5deb43cc8
Michael Tarleton <michael.tarleton@me.com>
2018-12-25 21:32:37 -0600

    master: Init angular material module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common'
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
+import { AngularMaterialModule } from './angular-material/angular-material.module'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, AngularMaterialModule],
   exports: [CommonModule],
   declarations: [],
 })
