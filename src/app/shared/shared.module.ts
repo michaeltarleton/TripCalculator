2fa1b2d03109032829bec0253bd7b30111ca89ad
Michael Tarleton <michael.tarleton@me.com>
2018-12-25 21:31:03 -0600

    master: Added routing and made some linting fixes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule],
+  exports: [CommonModule],
+  declarations: [],
 })
-export class SharedModule { }
+export class SharedModule {}
