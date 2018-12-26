fdc8b02670d685e24eb581cd40c722ec32baef9f
Michael Tarleton <michael.tarleton@me.com>
2018-12-25 22:25:38 -0600

    master: Fixed linting errors

--- src/app/core/navbar/navbar.component.spec.ts
+++ src/app/core/navbar/navbar.component.spec.ts
@@ -1,6 +1,5 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatButtonModule,
   MatIconModule,
@@ -8,6 +7,7 @@ import {
   MatSidenavModule,
   MatToolbarModule,
 } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NavbarComponent } from './navbar.component';
 
--- src/app/modules/friend-list-dashboard/friend-list-dashboard.component.spec.ts
+++ src/app/modules/friend-list-dashboard/friend-list-dashboard.component.spec.ts
@@ -1,5 +1,4 @@
 import { LayoutModule } from '@angular/cdk/layout';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {
   MatButtonModule,
@@ -8,6 +7,7 @@ import {
   MatIconModule,
   MatMenuModule,
 } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FriendListDashboardComponent } from './friend-list-dashboard.component';
 
