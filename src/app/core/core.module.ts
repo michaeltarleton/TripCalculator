85407869e5253ac4fc8a8eb2343edbce2b4403de
Michael Tarleton <michael.tarleton@me.com>
2018-12-25 21:47:35 -0600

    master: Init navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core'
 
-import { SharedModule } from '../shared/shared.module'
+import { SharedModule } from '../shared/shared.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material'
 
 @NgModule({
-  imports: [SharedModule],
+  imports: [SharedModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   exports: [SharedModule],
+  declarations: [NavbarComponent],
 })
 export class CoreModule {}
--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core'
+import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-const modules: ReadonlyArray<any> = [BrowserAnimationsModule]
+const modules: ReadonlyArray<any> = [
+  BrowserAnimationsModule,
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+]
 
 @NgModule({
   imports: [...modules],
